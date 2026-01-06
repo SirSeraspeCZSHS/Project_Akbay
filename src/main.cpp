31817dc598c27be62835f01459287705422ba2f9
miguelcyber <rabanalmiguel058@gmail.com>
2026-01-06 15:18:45 +0800

    MIGUEL_SHOULDER_ABDUCTION_FLEXION_ELBOW

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,21 @@
 const int LED_PIN = 2;
 
 // Set these pins to match your wiring
-#define stepperYStep_pin 25 // pin for stepper Y axis' driver
-#define stepperYDir_pin 27 // pin for stepper Y axis' direction
 
-AccelStepper stepperY(AccelStepper::DRIVER, stepperYStep_pin, stepperYDir_pin); // create an object for stepper X axis
+#define stepperX_elbow_Step_pin 26 // pin for stepper X axis' driver
+#define stepperX_elbow_Dir_pin 16 // pin for stepper X axis' direction/dating 16
 
-void setup() {
+#define stepperY_shoulder_flexion_Step_pin 25 // pin for stepper Y axis' driver
+#define stepperY_shoulder_flexion_Dir_pin 27 // pin for stepper Y axis' direction
+
+#define stepperZ_shoulder_abduction_Step_pin 17 // pin for stepper Z axis' driver, dating 17
+#define stepperZ_shoulder_abduction_Dir_pin 14 // pin for stepper Z axis' direction
+
+
+AccelStepper stepperX(AccelStepper::DRIVER, stepperX_elbow_Step_pin, stepperX_elbow_Dir_pin); 
+AccelStepper stepperY(AccelStepper::DRIVER, stepperY_shoulder_flexion_Step_pin, stepperY_shoulder_flexion_Dir_pin); 
+AccelStepper stepperZ(AccelStepper::DRIVER, stepperZ_shoulder_abduction_Step_pin, stepperZ_shoulder_abduction_Dir_pin);
+
   Serial.begin(115200);
   pinMode(LED_PIN, OUTPUT);
   Serial.println("ESP32 PlatformIO AccelStepper example");
